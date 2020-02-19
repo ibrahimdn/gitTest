8559720ceb63e79e2ff5b33a039f5ed4482c7b05
İbrahim DOĞAN <ibrahim.dogan@btpro.net>
2020-02-19 09:45:53 +0300

    Initial Commit

---
+++ clickTextView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clickTextView
+//
+//  Created by İbrahim DOĞAN on 2/19/20.
+//  Copyright © 2020 İbrahim DOĞAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
