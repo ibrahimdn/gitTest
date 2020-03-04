3ca39530e1ebedaf14b95f251039198fc1569541
İbrahim DOĞAN <ibrahim.dogan@btpro.net>
2020-03-04 15:57:23 +0300

    3. commit

--- clickTextView/ViewController.swift
+++ clickTextView/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("2. commit")
+        print("3. commit")
     }
     
     
